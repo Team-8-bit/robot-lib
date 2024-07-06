9abbfd31a206369251b80656b0b35e2edaa6ca9b
nolan <nolan.s.locke@gmail.com>
2024-07-06 14:49:43 +0200

    add Command.toAction() utility

--- robot-lib/src/main/kotlin/org/team9432/lib/resource/Action.kt
+++ robot-lib/src/main/kotlin/org/team9432/lib/resource/Action.kt
@@ -1,5 +1,32 @@
 package org.team9432.lib.resource
 
+import edu.wpi.first.wpilibj2.command.Command
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.suspendCancellableCoroutine
+import org.team9432.lib.robot.CoroutineRobot
+import kotlin.coroutines.resume
 
 typealias Action = suspend CoroutineScope.() -> Unit
+
+fun Command.toAction(): Action {
+    val command = this
+    return {
+        suspendCancellableCoroutine { cont ->
+            command.initialize()
+
+            val periodic = CoroutineRobot.startPeriodic {
+                command.execute()
+                if (command.isFinished) {
+                    command.end(false)
+                    this.stopPeriodic()
+                    cont.resume(Unit)
+                }
+            }
+
+            cont.invokeOnCancellation {
+                command.end(true)
+                periodic.stopPeriodic()
+            }
+        }
+    }
+}
