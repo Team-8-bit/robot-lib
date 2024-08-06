bccd2a709b5b32e4728729f6990e36a38180e22a
nolan <nolan.s.locke@gmail.com>
2024-08-06 13:25:49 -0700

    move some more geometry utilities from robot code

--- robot-lib/src/main/kotlin/org/team9432/lib/unit/WPIGeometry.kt
+++ robot-lib/src/main/kotlin/org/team9432/lib/unit/WPIGeometry.kt
@@ -7,5 +7,6 @@ import edu.wpi.first.math.geometry.Translation2d
 // Constructors for wpilib geometry classes that use the units library. Added to as needed.
 fun Pose2d(x: Length, y: Length, rotation: Rotation2d) = Pose2d(x.inMeters, y.inMeters, rotation)
 fun Pose2d(x: Length, y: Length, rotation: Angle) = Pose2d(x.inMeters, y.inMeters, rotation.asRotation2d)
+fun Pose2d(x: Double, y: Double, rotation: Angle) = Pose2d(x, y, rotation.asRotation2d)
 fun Pose2d(translation: Translation2d, rotation: Angle) = Pose2d(translation, rotation.asRotation2d)
 fun Translation2d(x: Length, y: Length) = Translation2d(x.inMeters, y.inMeters)
--- robot-lib/src/main/kotlin/org/team9432/lib/util/GeometryUtil.kt
+++ robot-lib/src/main/kotlin/org/team9432/lib/util/GeometryUtil.kt
@@ -4,8 +4,9 @@ import edu.wpi.first.math.geometry.Pose2d
 import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.math.geometry.Translation2d
 import org.team9432.lib.constants.EvergreenFieldConstants
-import org.team9432.lib.unit.Translation2d
-import org.team9432.lib.unit.meters
+import org.team9432.lib.unit.*
+import kotlin.math.atan2
+import kotlin.math.hypot
 
 /** Flips this [Pose2d] to the opposite side of a mirrored field. */
 fun Pose2d.flip() = Pose2d(translation.flip(), rotation.flip())
@@ -24,3 +25,28 @@ fun Translation2d.applyFlip() = allianceSwitch(blue = this, red = this.flip())
 
 /** Flips this [Rotation2d] to the correct side of a mirrored field based on the current alliance color. */
 fun Rotation2d.applyFlip() = allianceSwitch(blue = this, red = this.flip())
+
+
+/** Returns the angle this pose would need to be at to point at the given pose. */
+fun Translation2d.angleTo(pose: Translation2d) = atan2(pose.y - this.y, pose.x - this.x).radians
+
+/** Returns the angle this pose would need to be at to point at the given pose. */
+fun Pose2d.angleTo(pose: Pose2d) = atan2(pose.y - this.y, pose.x - this.x).radians
+
+/** Returns true if this pose is within [epsilon] of the given pose. */
+fun Translation2d.isNear(pose: Translation2d, epsilon: Length) = hypot(this.x - pose.x, this.y - pose.y) < epsilon.inMeters
+
+/** Returns true if this pose is within [epsilon] of the given pose. */
+fun Pose2d.isNear(pose: Pose2d, epsilon: Length) = hypot(this.x - pose.x, this.y - pose.y) < epsilon.inMeters
+
+/** Return the distance from this pose to another. */
+fun Translation2d.distanceTo(pose: Translation2d) = this.getDistance(pose).meters
+
+/** Return the distance from this pose to another. */
+fun Pose2d.distanceTo(pose: Pose2d) = this.translation.getDistance(pose.translation).meters
+
+/** Returns a Pose2d at this translation with rotation to point at the given pose. */
+fun Translation2d.pointAt(pose: Translation2d) = Pose2d(x, y, this.angleTo(pose))
+
+/** Returns a Pose2d at this translation with rotation to point at the given pose. */
+fun Translation2d.pointAt(pose: Pose2d) = Pose2d(x, y, this.angleTo(pose.translation))
