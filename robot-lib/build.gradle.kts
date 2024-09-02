ecd63130494289c7a624ab4782591e2b3b9a2545
Nolan L <nolan.s.locke@gmail.com>
2024-09-02 13:28:35 -0700

    add advantagekit dependency

--- robot-lib/build.gradle.kts
+++ robot-lib/build.gradle.kts
@@ -2,6 +2,7 @@ val kotlinVersion: String by project
 val kspVersion: String by project
 val wpiLibVersion: String by project
 val frcYear: String by project
+val advantageKitVersion: String by project
 
 plugins {
     kotlin("jvm")
@@ -10,6 +11,12 @@ plugins {
 group = "org.team9432.lib"
 
 dependencies {
+    // AdvantageKit
+    implementation("org.littletonrobotics.akit.junction:junction-core:$advantageKitVersion")
+    implementation("org.littletonrobotics.akit.junction:wpilib-shim:$advantageKitVersion")
+    implementation("org.littletonrobotics.akit.conduit:conduit-api:$advantageKitVersion")
+    implementation("org.littletonrobotics.akit.conduit:conduit-wpilibio:$advantageKitVersion")
+
     // Wpilib
     implementation("edu.wpi.first.hal:hal-java:$wpiLibVersion")
     implementation("edu.wpi.first.wpilibj:wpilibj-java:$wpiLibVersion")
@@ -50,7 +57,20 @@ repositories {
     maven { setUrl("https://maven.photonvision.org/repository/snapshots") }
     maven { setUrl("https://jitpack.io") }
     maven { setUrl("https://SleipnirGroup.github.io/ChoreoLib/dep") }
+    maven {
+        url = uri("https://maven.pkg.github.com/Mechanical-Advantage/AdvantageKit")
+        credentials {
+            username = "Mechanical-Advantage-Bot"
+            password = "\u0067\u0068\u0070\u005f\u006e\u0056\u0051\u006a\u0055\u004f\u004c\u0061\u0079\u0066\u006e\u0078\u006e\u0037\u0051\u0049\u0054\u0042\u0032\u004c\u004a\u006d\u0055\u0070\u0073\u0031\u006d\u0037\u004c\u005a\u0030\u0076\u0062\u0070\u0063\u0051"
+        }
+    }
 }
+
+
+configurations.all {
+    exclude(group = "edu.wpi.first.wpilibj")
+}
+
 
 sourceSets.main {
     java.srcDirs("src/main/kotlin")
---
+++ robot-lib/src/main/kotlin/org/team9432/lib/LoggedCoroutineRobot.kt
@@ -0,0 +1,2 @@
+package org.team9432.lib
+
