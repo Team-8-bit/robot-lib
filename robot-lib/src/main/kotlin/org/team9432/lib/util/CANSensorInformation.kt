d6a6f09499ee0fb236108a90006d7ad8e69d9d71
Nolan L <nolan.s.locke@gmail.com>
2024-12-28 23:08:34 -0700

    add motor information data classes

---
+++ robot-lib/src/main/kotlin/org/team9432/lib/util/CANSensorInformation.kt
@@ -0,0 +1,6 @@
+package org.team9432.lib.util
+
+data class CANSensorInformation(
+    val canID: Int,
+    val canBus: String,
+)
---
+++ robot-lib/src/main/kotlin/org/team9432/lib/util/MotorInformation.kt
@@ -0,0 +1,7 @@
+package org.team9432.lib.util
+
+data class MotorInformation(
+    val canID: Int,
+    val pdhChannel: Int,
+    val canBus: String,
+)
