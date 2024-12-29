e50985f2d75b4ba2c73b13127ceaf6c123b395df
Nolan L <nolan.s.locke@gmail.com>
2024-12-29 00:46:27 -0700

    remove some unused utilities

--- robot-lib/src/main/kotlin/org/team9432/lib/util/ChassisSpeedsUtil.kt
+++ robot-lib/src/main/kotlin/org/team9432/lib/util/ChassisSpeedsUtil.kt
@@ -1,9 +1,8 @@
 package org.team9432.lib.util
 
 import edu.wpi.first.math.kinematics.ChassisSpeeds
-import edu.wpi.first.math.util.Units
 import kotlin.math.abs
 import kotlin.math.hypot
 
-fun ChassisSpeeds.velocityLessThan(metersPerSecond: Double, rotationsPerSecond: Double) =
-    hypot(vxMetersPerSecond, vyMetersPerSecond) < metersPerSecond && abs(Units.radiansToRotations(omegaRadiansPerSecond)) < rotationsPerSecond
+fun ChassisSpeeds.velocityLessThan(metersPerSecond: Double, radiansPerSecond: Double) =
+    hypot(vxMetersPerSecond, vyMetersPerSecond) < metersPerSecond && abs(omegaRadiansPerSecond) < radiansPerSecond
--- robot-lib/src/main/kotlin/org/team9432/lib/util/SparkBase.kt
+++ robot-lib/src/main/kotlin/org/team9432/lib/util/SparkBase.kt
@@ -2,4 +2,5 @@ package org.team9432.lib.util
 
 import com.revrobotics.spark.SparkBase
 
+/** Returns the temperature if this device in fahrenheit. */
 val SparkBase.temperatureFahrenheit get() = (motorTemperature * (9 / 5)) + 32
